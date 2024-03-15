b36bd48467903bf8754679932716b882efb5f0ab
小川智裕 <tomo.create30@gmail.com>
2024-03-15 15:48:39 +0900

    fix: VとTを作成

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=100)
+    description = models.Textfield(null=True,blank=True)
+    completed = models.BooleanField(default=True)
+    createdDate = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering= ["completed"]
