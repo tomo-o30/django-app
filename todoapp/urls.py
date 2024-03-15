295d221f3648aeacbb2ce00e6d3f988a54327a50
tomo-o30 <tomo.ogawa18@gmail.com>
2024-03-15 19:09:24 +0900

    add: create機能追加

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import TaskList, TaskDetail
+from .views import TaskCreate, TaskList, TaskDetail
 
 urlpatterns = [
-    path("", TaskList.as_view()),
-    path("task/<int:pk>/", TaskDetail.as_view(), name="task")
+    path("", TaskList.as_view(), name = "tasks"),
+    path("task/<int:pk>/", TaskDetail.as_view(), name="task"),
+    path("create_task/", TaskCreate.as_view(), name="create-task")
 ]
 
 
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from todoapp.models import Task
 
 
@@ -12,3 +14,7 @@ class TaskDetail(DetailView):
     model= Task
     context_object_name = "task"
     
+class TaskCreate(CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks")
